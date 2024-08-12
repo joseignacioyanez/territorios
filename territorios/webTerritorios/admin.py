edc9e5c231a336c2e4bb5b516b43a2c25d28b045
Jose Ignacio <joseignacioyanez@hotmail.com>
2024-08-11 22:13:20 -0500

    Add Export Admin Dependency XLSX and fix properties on Admin

--- territorios/webTerritorios/admin.py
+++ territorios/webTerritorios/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django import forms
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from import_export.admin import ImportExportModelAdmin
 
 from.models import *
 
 
-class SordoAdmin(admin.ModelAdmin):
+class SordoAdmin(ImportExportModelAdmin):
     exclude = ('codigo', 'local_id')
     ordering = ('codigo',)
     list_filter = ('congregacion', 'estado_sordo', 'territorio', 'publicador_estudio')
@@ -21,6 +22,9 @@ class SordoAdmin(admin.ModelAdmin):
             }
         return super().get_form(request, obj, **kwargs)
     
+class TerritorioAdmin(ImportExportModelAdmin):
+    list_display = ('numero', 'nombre', 'congregacion')
+    
 
 # Manejar Publicador como extension de User
 class PublicadorInline(admin.StackedInline):
@@ -28,8 +32,13 @@ class PublicadorInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'Publicadores'
 
+class AsignacionAdmin(ImportExportModelAdmin):
+    list_display = ('publicador', 'territorio', 'fecha_asignacion', 'fecha_fin')
+
 # Define a new User admin
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(BaseUserAdmin, ImportExportModelAdmin):
+    list_filter = ('is_staff', 'publicador__congregacion', 'publicador__activo')
+    list_display = ('username', 'is_staff')
     inlines = [PublicadorInline]
 
 # Re-register UserAdmin
@@ -40,5 +49,5 @@ admin.site.register(User, UserAdmin)
 admin.site.register(Sordo, SordoAdmin)
 admin.site.register(Congregacion)
 admin.site.register(EstadoSordo)
-admin.site.register(Territorio)
-admin.site.register(Asignacion)
+admin.site.register(Territorio, TerritorioAdmin)
+admin.site.register(Asignacion, AsignacionAdmin)
